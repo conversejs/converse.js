2708d976db47f9d3b411d20637d065cc93e2e15f
JC Brand <jc@opkode.com>
2013-04-26 21:50:57 +0200

    typo and comment

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require(["jquery", "converse"], function($, converse) {
     converse.initialize({
         animate: true,
-        bosh_service_url: 'https://bind.opkode.im',
+        bosh_service_url: 'https://bind.opkode.im', // Please use this connection manager only for testing purposes
         prebind: false,
         xhr_user_search: false,
         auto_subscribe: false 

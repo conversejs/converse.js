6d9fe68a2534f1a220293a09ea9fdabe1eccc12f
JC Brand <jc@opkode.com>
2013-05-11 14:20:07 +0200

    Split ChatRooms tests out into their own spec file

--- tests_main.js
+++ tests_main.js
@@ -1,16 +1,21 @@
-require(["jquery", "spec/MainSpec"], function($) {
-
-    $(function($) {
-        var jasmineEnv = jasmine.getEnv();
-        jasmineEnv.updateInterval = 250;
-
-        var htmlReporter = new jasmine.HtmlReporter();
-
-        jasmineEnv.addReporter(htmlReporter);
-
-        jasmineEnv.specFilter = function(spec) {
-            return htmlReporter.specFilter(spec);
-        };
-        jasmineEnv.execute();
+require(["jquery", "converse", "mock", "spec/MainSpec", "spec/ChatRoomSpec"], function($, converse, mock_connection) {
+    // Set up converse.js
+    window.localStorage.clear();
+    converse.initialize({
+        prebind: false,
+        xhr_user_search: false,
+        auto_subscribe: false,
+        animate: false
     });
+    converse.onConnected(mock_connection);
+
+    // Jasmine stuff
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 50;
+    var htmlReporter = new jasmine.HtmlReporter();
+    jasmineEnv.addReporter(htmlReporter);
+    jasmineEnv.specFilter = function(spec) {
+        return htmlReporter.specFilter(spec);
+    };
+    jasmineEnv.execute();
 });

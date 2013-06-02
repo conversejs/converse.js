4d9b9a7ba4ce46fb4d3152cfcc05a69b3abd1b03
JC Brand <jc@opkode.com>
2013-06-02 19:38:11 +0200

    Move locales.js to locale dir
    Trying to make it simpler and easier for people to add new translations by
    reducing the amount of places where edits need to be made (e.g. no new edits
    required in converse.js now).

--- locale/locales.js
+++ locale/locales.js
@@ -1,4 +1,13 @@
 (function (root, factory) {
+    require.config({
+        paths: {
+            "jed": "Libraries/jed",
+            "af": "locale/af/LC_MESSAGES/af",
+            "en": "locale/en/LC_MESSAGES/en",
+            "de": "locale/de/LC_MESSAGES/de"
+        }
+    });
+
     define("locales", [
         'jed',
         'af',

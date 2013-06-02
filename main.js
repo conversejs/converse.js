895df5fff5c0816fe054fa8f8bb58a0c693881bf
JC Brand <jc@opkode.com>
2013-06-02 19:59:21 +0200

    Add default config values to main.js (for reference)

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 require(["jquery", "converse"], function($, converse) {
+    // Most of these initialization values are the defaults but they're
+    // included here as a reference.
     converse.initialize({
-        bosh_service_url: 'https://bind.opkode.im' // Please use this connection manager only for testing purposes
+        auto_list_rooms: false,
+        auto_subscribe: false,
+        bosh_service_url: 'https://bind.opkode.im', // Please use this connection manager only for testing purposes
+        hide_muc_server: false,
+        i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
+        prebind: false,
+        xhr_user_search: false
     });
 });

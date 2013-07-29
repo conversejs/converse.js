7ed30139d60b04b0161bb7d8c03035197bc07d6d
JC Brand <jc@opkode.com>
2013-07-29 22:34:33 +0200

    Move path and shim configs from converse.js to main.js
    This should make it easier to create custom builds, e.g. with more or less
    dependencies.

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+require.config({
+    paths: {
+        "jquery": "components/jquery/jquery",
+        "locales": "locale/locales",
+        "sjcl": "components/sjcl/sjcl",
+        "tinysort": "components/tinysort/src/jquery.tinysort",
+        "underscore": "components/underscore/underscore",
+        "backbone": "components/backbone/backbone",
+        "localstorage": "components/backbone.localStorage/backbone.localStorage",
+        "strophe": "components/strophe/strophe",
+        "strophe.muc": "components/strophe.muc/index",
+        "strophe.roster": "components/strophe.roster/index",
+        "strophe.vcard": "components/strophe.vcard/index",
+        "strophe.disco": "components/strophe.disco/index"
+    },
+
+    // define module dependencies for modules not using define
+    shim: {
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: [
+                'underscore',
+                'jquery'
+                ],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'tinysort': { deps: ['jquery'] },
+        'strophe': { deps: ['jquery'] },
+        'underscore':   { exports: '_' },
+        'strophe.muc':  { deps: ['strophe', 'jquery'] },
+        'strophe.roster':   { deps: ['strophe'] },
+        'strophe.vcard':    { deps: ['strophe'] },
+        'strophe.disco':    { deps: ['strophe'] }
+    }
+});
+
 require(["jquery", "converse"], function($, converse) {
     window.converse = converse;
 });

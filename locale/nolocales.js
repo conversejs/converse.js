b61c936790b7d1fb2d5f228b7b1828a2e803650c
JC Brand <jc@opkode.com>
2013-08-27 09:59:14 +0200

    Don't reference LC_MESSAGES/en Bungle doesn't seem to find it.

--- locale/nolocales.js
+++ locale/nolocales.js
@@ -2,10 +2,19 @@
  * This file can be used if no locale support is required.
  */
 (function (root, factory) {
-    define("locales", [
-            'jed',
-            "locale/en/LC_MESSAGES/en"
-        ], function (jed, en) {
-            root.locales = { 'en': en };
+    define("locales", ['jed'], function (jed) {
+        var translations = {
+            "domain": "converse",
+            "locale_data": {
+                "converse": {
+                    "": {
+                        "domain": "converse",
+                        "lang": "en",
+                        "plural_forms": "nplurals=2; plural=(n != 1);"
+                    }
+                }
+            }
+        };
+        root.locales = { 'en': factory(new Jed(translations)) };
     });
 })(this);

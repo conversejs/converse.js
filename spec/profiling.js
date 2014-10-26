015367af6242bb104f6ba7c6b56ec15a2460eaf1
JC Brand <jc@opkode.com>
2014-10-26 17:12:59 +0100

    Add another profiling method for adding users without groups

--- spec/profiling.js
+++ spec/profiling.js
@@ -9,22 +9,15 @@
     );
 } (this, function ($, mock, test_utils) {
     describe("Profiling", function() {
-        var roster;
         beforeEach(function() {
-            roster = converse.connection.roster;
+            converse.connection.roster.items = [];
             converse.connection._changeConnectStatus(Strophe.Status.CONNECTED);
         });
 
         it("adds hundreds of contacts to the roster", $.proxy(function() {
-        }, converse));
-
-        it("adds hundreds of contacts to the roster, with roster groups", $.proxy(function() {
-            // XXX: Try with groups for now (might also add a test without groups)
-            converse.roster_groups = true;
-
+            converse.roster_groups = false;
             spyOn(this.roster, 'clearCache').andCallThrough();
             expect(this.roster.pluck('jid').length).toBe(0);
-
             var stanza = $iq({
                 to: this.connection.jid,
                 type: 'result',
@@ -32,7 +25,6 @@
             }).c('query', {
                 xmlns: 'jabber:iq:roster'
             });
-
             _.each(['Friends', 'Colleagues', 'Family', 'Acquaintances'], function (group) {
                 var i;
                 for (i=0; i<100; i++) {
@@ -44,7 +36,31 @@
             });
             this.connection.roster._onReceiveRosterSuccess(null, stanza.tree());
             expect(this.roster.clearCache).toHaveBeenCalled();
+            expect(this.roster.pluck('jid').length).toBe(400);
+        }, converse));
 
+        it("adds hundreds of contacts to the roster, with roster groups", $.proxy(function() {
+            converse.roster_groups = true;
+            spyOn(this.roster, 'clearCache').andCallThrough();
+            expect(this.roster.pluck('jid').length).toBe(0);
+            var stanza = $iq({
+                to: this.connection.jid,
+                type: 'result',
+                id: 'roster_1'
+            }).c('query', {
+                xmlns: 'jabber:iq:roster'
+            });
+            _.each(['Friends', 'Colleagues', 'Family', 'Acquaintances'], function (group) {
+                var i;
+                for (i=0; i<100; i++) {
+                    stanza = stanza.c('item', {
+                        jid: Math.random().toString().replace('0.', '')+'@example.net',
+                        subscription:'both'
+                    }).c('group').t(group).up().up();
+                }
+            });
+            this.connection.roster._onReceiveRosterSuccess(null, stanza.tree());
+            expect(this.roster.clearCache).toHaveBeenCalled();
             expect(this.roster.pluck('jid').length).toBe(400);
         }, converse));
 

57d903fd9725285122d4afa025327d7d048bfc5b
JC Brand <jc@opkode.com>
2014-10-30 15:24:32 +0100

    Remove config settings which are already in main.js file.

--- src/build-no-jquery.js
+++ src/build-no-jquery.js
@@ -3,16 +3,10 @@
     name: "components/almond/almond.js",
     out: "../builds/converse.nojquery.min.js",
     include: ['main'],
-    tpl: {
-        // Use Mustache style syntax for variable interpolation
-        templateSettings: {
-            evaluate : /\{\[([\s\S]+?)\]\}/g,
-            interpolate : /\{\{([\s\S]+?)\}\}/g
-        }
-    },
     mainConfigFile: '../main.js',
     paths: {
         "converse-dependencies":    "src/deps-full",
-        "jquery":                   "src/jquery-external"
+        "jquery":                   "src/jquery-external",
+        "jquery-private":           "src/jquery-external-private"
     }
 })

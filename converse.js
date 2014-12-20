fadbf2d15931776678be5a63d54e872768e13c5b
JC Brand <jc@opkode.com>
2014-12-20 10:17:20 +0100

    JSHint bitching about wrapped or statements

--- converse.js
+++ converse.js
@@ -3228,12 +3228,9 @@
 
             showInRoster: function () {
                 var chatStatus = this.get('chat_status');
-                if ((converse.show_only_online_users && chatStatus !== 'online')
-                    || (converse.hide_offline_users && chatStatus === 'offline')) {
+                if ((converse.show_only_online_users && chatStatus !== 'online') || (converse.hide_offline_users && chatStatus === 'offline')) {
                     // If pending or requesting, show
-                    if ((this.get('ask') === 'subscribe')
-                        || (this.get('subscription') === 'from')
-                        || (this.get('requesting') === true)) {
+                    if ((this.get('ask') === 'subscribe') || (this.get('subscription') === 'from') || (this.get('requesting') === true)) {
                         return true;
                     }
                     return false;

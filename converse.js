1390e1a3b044d45be83ed4f85bcd145bd842d193
JC Brand <jc@opkode.com>
2015-08-24 17:01:55 +0200

    3 changes
    - updated version of moment.js
    - remove unused variables in converse.js
    - fix broken link in index.html

--- converse.js
+++ converse.js
@@ -4581,7 +4581,7 @@
                  * If all contacts are filtered out (i.e. hidden), then the
                  * group must be filtered out as well.
                  */
-                var matches, rejects;
+                var matches;
                 if (q.length === 0) {
                     if (this.model.get('state') === OPENED) {
                         this.model.contacts.each(function (item) {
@@ -4790,7 +4790,6 @@
             },
 
             filter: function (query, type) {
-                var matches;
                 query = query.toLowerCase();
                 if (type === 'groups') {
                     _.each(this.getAll(), function (view, idx) {

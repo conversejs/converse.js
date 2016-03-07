c2212833a1aa8553285d50428ca84fd9aeaaf722
JC Brand <jc@opkode.com>
2016-03-07 14:29:45 +0000

    Fixes #607, view.model is undefined.

--- src/converse-core.js
+++ src/converse-core.js
@@ -2467,7 +2467,9 @@
                     boxes_width = newchat ? newchat.$el.outerWidth(true) : 0,
                     new_id = newchat ? newchat.model.get('id') : null;
 
-                boxes_width += _.reduce(this.xget(new_id), this.getChatBoxWidth.bind(this));
+                boxes_width += _.reduce(this.xget(new_id), function (memo, view) {
+                    return memo + this.getChatBoxWidth(view);
+                }.bind(this), 0);
 
                 if ((minimized_width + boxes_width) > $('body').outerWidth(true)) {
                     oldest_chat = this.getOldestMaximizedChat([new_id]);

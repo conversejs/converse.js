562989dea16f89a827452ecd5ecd81880e34de9b
theSouL <theSouL@users.noreply.github.com>
2017-03-08 21:12:41 +0100

    Add comment

--- src/converse-core.js
+++ src/converse-core.js
@@ -1479,7 +1479,11 @@
             onMessage: function (message) {
                 /* Handler method for all incoming single-user chat "message"
                  * stanzas.
-                 */
+                 */          
+                //How can I propagate an element found in message.childNodes until the message is displayed on the chat?
+                //I've seen in some functions, message's attributes are used. Should I setAttribute() to the message
+                //to be able to check the element's information later?
+                
                 var original_stanza = message,
                     contact_jid, forwarded, delay, from_bare_jid,
                     from_resource, is_me, msgid,

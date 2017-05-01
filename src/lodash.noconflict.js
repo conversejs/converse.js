738d227297afee03f6558ff987a52c6f7f896f2e
JC Brand <jc@opkode.com>
2017-05-01 12:40:29 +0200

    Solve the intermittent issue of _ not being defined in templates

--- src/lodash.noconflict.js
+++ src/lodash.noconflict.js
@@ -1,4 +1,17 @@
 /*global define */
 define(['lodash'], function (_) {
+    if (!_.isUndefined(require)) {
+        /* XXX: This is a hack to make sure that the compiled templates have
+         * access to the _ object.
+         *
+         * Otherwise we sometimes get errors like this:
+         *
+         *    TypeError: Cannot read property 'escape' of undefined
+         *     at eval (./src/templates/chatroom_sidebar.html:6)
+         */
+        var lodashLoader = require.s.contexts._.config.lodashLoader;
+        lodashLoader.templateSettings.imports = { '_': _ };
+        require.config({'lodashLoader': lodashLoader});
+    }
     return _.noConflict();
 });

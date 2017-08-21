ab0ec7301f39e9d60d84b8db4d6d466848aa6f6d
JC Brand <jc@opkode.com>
2017-08-21 15:41:52 +0200

    Various fixes for browser that don't support ES2015
    Specifically for IE11.
    
    * Add a polyfill for String.prototype.includes
    * Make sure all bundled code is transpiled
    * Add IE specific fix in converse-otr.js

--- src/lodash.fp.js
+++ src/lodash.fp.js
@@ -1,5 +1,5 @@
 define(['lodash', 'lodash.converter', 'converse-core'], function (_, lodashConverter, converse) {
-    const fp = lodashConverter(_.runInContext());
+    var fp = lodashConverter(_.runInContext());
     converse.env.fp = fp;
     return fp;
 });

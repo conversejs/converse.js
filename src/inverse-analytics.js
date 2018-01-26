8c1e15c3f14de0df75d9145425d0ce1fbbaf67e4
gnunicode <34939282+gnunicode@users.noreply.github.com>
2018-01-26 19:59:59 +0000

    Add files via upload

---
+++ src/inverse-analytics.js
@@ -0,0 +1,11 @@
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.opkode.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '5']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();

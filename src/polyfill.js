0a28e7aae4d0cf86ee2cded20e4a53c1d69235e0
JC Brand <jc@opkode.com>
2018-03-02 15:51:04 +0100

    Fixes #1027 Backwards compatibility fix for IE11

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,3 +1,14 @@
+function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+}
+if ( typeof window.CustomEvent !== "function" ) {
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent;
+}
+
 if (!String.prototype.includes) {
   String.prototype.includes = function(search, start) {
         'use strict';

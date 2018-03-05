de66a7ddfa74c00a3bb3b2ae3fe7f046b6e3e1e0
JC Brand <jc@opkode.com>
2018-03-05 15:12:29 +0100

    Cleanup post release

--- dist/converse-muc-embedded.js
+++ dist/converse-muc-embedded.js
@@ -41763,8 +41763,7 @@ return Backbone.BrowserStorage;
   return Backbone.Overview;
 });
 
-//# sourceMappingURL=backbone.overview.js.map
-;
+//# sourceMappingURL=backbone.overview.js.map;
 // Converse.js (A browser based XMPP chat client)
 // http://conversejs.org
 //
@@ -48800,14 +48799,10 @@ define("awesomplete", (function (global) {
       var item_view = this.get(item.get(this.subviewIndex));
 
       if (!item_view) {
-            try {
-                item_view = new this.ItemView({model: item});
-                this.add(item.get(this.subviewIndex), item_view);
-            } catch (e) {
-                debugger;
-                item_view = new this.ItemView({model: item});
-                this.add(item.get(this.subviewIndex), item_view);
-            }
+        item_view = new this.ItemView({
+          model: item
+        });
+        this.add(item.get(this.subviewIndex), item_view);
       } else {
         item_view.model = item;
         item_view.initialize();
@@ -48845,8 +48840,7 @@ define("awesomplete", (function (global) {
   return Backbone.OrderedListView;
 });
 
-//# sourceMappingURL=backbone.orderedlistview.js.map
-;
+//# sourceMappingURL=backbone.orderedlistview.js.map;
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define('snabbdom',[],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.snabbdom = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });

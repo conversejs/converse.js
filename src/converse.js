14b9c5792d9af2a594adbbb20c09ee7cc4379b87
JC Brand <jc@opkode.com>
2018-10-23 15:32:08 +0200

    Remove unused files

--- src/converse.js
+++ src/converse.js
@@ -1,35 +1,29 @@
-/*global define */
-if (typeof define !== 'undefined') {
-    // The section below determines which plugins will be included in a build
-    define([
-        "@converse/headless/converse-core",
-        /* START: Removable components
-         * --------------------
-         * Any of the following components may be removed if they're not needed.
-         */
-        "converse-autocomplete",
-        "converse-bookmarks",       // XEP-0048 Bookmarks
-        "converse-caps",            // XEP-0115 Entity Capabilities
-        "converse-chatview",        // Renders standalone chat boxes for single user chat
-        "converse-controlbox",      // The control box
-        "converse-dragresize",      // Allows chat boxes to be resized by dragging them
-        "converse-embedded",
-        "converse-fullscreen",
-        "converse-push",            // XEP-0357 Push Notifications
-        "converse-headline",        // Support for headline messages
-        "@converse/headless/converse-mam",             // XEP-0313 Message Archive Management
-        "converse-minimize",        // Allows chat boxes to be minimized
-        "@converse/headless/converse-muc",             // XEP-0045 Multi-user chat
-        "converse-muc-views",       // Views related to MUC
-        "converse-notification",    // HTML5 Notifications
-        "converse-omemo",
-        "@converse/headless/converse-ping",            // XEP-0199 XMPP Ping
-        "converse-register",        // XEP-0077 In-band registration
-        "converse-roomslist",       // Show currently open chat rooms
-        "converse-roster",
-        "@converse/headless/converse-vcard",           // XEP-0054 VCard-temp
-        /* END: Removable components */
-    ], function (converse) {
-        return converse;
-    });
-}
+/* START: Removable components
+ * --------------------
+ * Any of the following components may be removed if they're not needed.
+ */
+import "converse-autocomplete";
+import "converse-bookmarks";       // XEP-0048 Bookmarks
+import "converse-caps";            // XEP-0115 Entity Capabilities
+import "converse-chatview";        // Renders standalone chat boxes for single user chat
+import "converse-controlbox";      // The control box
+import "converse-dragresize";      // Allows chat boxes to be resized by dragging them
+import "converse-embedded";
+import "converse-fullscreen";
+import "converse-push";            // XEP-0357 Push Notifications
+import "converse-headline";        // Support for headline messages
+import "@converse/headless/converse-mam";             // XEP-0313 Message Archive Management
+import "converse-minimize";        // Allows chat boxes to be minimized
+import "@converse/headless/converse-muc";             // XEP-0045 Multi-user chat
+import "converse-muc-views";       // Views related to MUC
+import "converse-notification";    // HTML5 Notifications
+import "converse-omemo";
+import "@converse/headless/converse-ping";            // XEP-0199 XMPP Ping
+import "converse-register";        // XEP-0077 In-band registration
+import "converse-roomslist";       // Show currently open chat rooms
+import "converse-roster";
+import "@converse/headless/converse-vcard";           // XEP-0054 VCard-temp
+/* END: Removable components */
+
+import converse from "@converse/headless/converse-core";
+export default converse;
--- src/end-no-jquery.frag
+++
@@ -1,4 +0,0 @@
-    define('jquery', [], function () { return jQuery; });
-    define('jquery.noconflict', [], function () { return jQuery; });
-    return require('converse');
-}));

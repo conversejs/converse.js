2cfea7cb51ecd8d47bc20931414171ebb893c68e
JC Brand <jc@opkode.com>
2018-10-28 23:21:27 +0100

    Add description for translateable text

--- src/converse-muc-views.js
+++ src/converse-muc-views.js
@@ -640,6 +640,7 @@ converse.plugins.add('converse-muc-views', {
                 } else if (current_affiliation === 'outcast') {
                     this.showChatEvent(__("%1$s has been banned from this groupchat", occupant.get('nick')))
                 } else if (current_affiliation === 'admin' || current_affiliation == 'owner') {
+                    // For example: AppleJack is now an (admin|owner) of this groupchat
                     this.showChatEvent(__('%1$s is now an %2$s of this groupchat', occupant.get('nick'), current_affiliation))
                 }
             },

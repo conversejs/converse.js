49ff6369c956a0be91058d920fd8aca170a35eaa
JC Brand <jc@opkode.com>
2020-05-13 14:26:58 +0200

    Split out dropdown base class

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -4,29 +4,15 @@ import { until } from 'lit-html/directives/until.js';
 import DOMNavigator from "../dom-navigator";
 import { converse } from "@converse/headless/converse-core";
 
-
 const u = converse.env.utils;
 
 
-export class Dropdown extends CustomElement {
+export class BaseDropdown extends CustomElement {
 
     static get properties () {
         return {
-            'items': { type: Array }
+            '': { type: Array }
         }
-    }
-
-    render () {
-        return html`
-            <div class="dropleft">
-                <button type="button" class="btn btn--transparent btn--standalone" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fa fa-bars only-icon"></i>
-                </button>
-                <div class="dropdown-menu">
-                    ${ this.items.map(b => until(b, '')) }
-                </div>
-            </div>
-        `;
     }
 
     firstUpdated () {
@@ -36,31 +22,10 @@ export class Dropdown extends CustomElement {
         this.dropdown.addEventListener('click', ev => this.toggleMenu(ev));
         this.dropdown.addEventListener('keyup', ev => this.handleKeyUp(ev));
         document.addEventListener('click', ev => !this.contains(ev.target) && this.hideMenu(ev));
-        this.initArrowNavigation();
-    }
-
-    initArrowNavigation () {
-        if (!this.navigator) {
-            const options = {
-                'selector': '.dropdown-item',
-                'onSelected': el => el.focus()
-            };
-            this.navigator = new DOMNavigator(this.menu, options);
-        }
-    }
-
-    enableArrowNavigation (ev) {
-        if (ev) {
-            ev.preventDefault();
-            ev.stopPropagation();
-        }
-        this.navigator.enable();
-        this.navigator.select(this.menu.firstElementChild);
     }
 
     hideMenu () {
         u.removeClass('show', this.menu);
-        this.navigator.disable();
         this.button.setAttribute('aria-expanded', false);
         this.button.blur();
     }
@@ -87,4 +52,64 @@ export class Dropdown extends CustomElement {
     }
 }
 
-window.customElements.define('converse-dropdown', Dropdown);
+
+export class DropdownList extends BaseDropdown {
+
+    static get properties () {
+        return {
+            'items': { type: Array }
+        }
+    }
+
+    render () {
+        return html`
+            <div class="dropleft">
+                <button type="button" class="btn btn--transparent btn--standalone" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-bars only-icon"></i>
+                </button>
+                <div class="dropdown-menu">
+                    ${ this.items.map(b => until(b, '')) }
+                </div>
+            </div>
+        `;
+    }
+
+    hideMenu () {
+        super.hideMenu();
+        this.navigator.disable();
+    }
+
+
+    firstUpdated () {
+        super.firstUpdated();
+        this.initArrowNavigation();
+    }
+
+    initArrowNavigation () {
+        if (!this.navigator) {
+            const options = {
+                'selector': '.dropdown-item',
+                'onSelected': el => el.focus()
+            };
+            this.navigator = new DOMNavigator(this.menu, options);
+        }
+    }
+
+    enableArrowNavigation (ev) {
+        if (ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+        }
+        this.navigator.enable();
+        this.navigator.select(this.menu.firstElementChild);
+    }
+
+    handleKeyUp (ev) {
+        super.handleKeyUp();
+        if (ev.keyCode === converse.keycodes.DOWN_ARROW && !this.navigator.enabled) {
+            this.enableArrowNavigation(ev);
+        }
+    }
+}
+
+window.customElements.define('converse-dropdown', DropdownList);

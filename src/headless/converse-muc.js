ffbab33a26bfc4bb15fa2811c161d6b148b17f22
JC Brand <jc@opkode.com>
2020-09-09 15:29:12 +0200

    Create own occupant before setting `connection_status` to ENTERED
    This makes it possible to wait for the connection_status before trying
    to call `getOwnOccupant` in plugins

--- src/headless/converse-muc.js
+++ src/headless/converse-muc.js
@@ -2339,9 +2339,15 @@ converse.plugins.add('converse-muc', {
              */
             onOwnPresence (stanza) {
                 if (stanza.getAttribute('type') !== 'unavailable') {
-                    this.session.save('connection_status', converse.ROOMSTATUS.ENTERED);
+                    // Set connection_status before creating the occupant, but
+                    // only trigger afterwards, so that plugins can access the
+                    // occupant in their event handlers.
+                    this.session.save('connection_status', converse.ROOMSTATUS.ENTERED, {'silent': true});
+                    this.updateOccupantsOnPresence(stanza);
+                    this.session.trigger('change:connection_status');
+                } else {
+                    this.updateOccupantsOnPresence(stanza);
                 }
-                this.updateOccupantsOnPresence(stanza);
 
                 if (stanza.getAttribute('type') === 'unavailable') {
                     this.handleDisconnection(stanza);

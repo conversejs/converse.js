819a83f2183c7a388effd608e985a29d9611da8a
JC Brand <jc@opkode.com>
2020-09-18 11:07:45 +0200

    Fix path to Strophe's shared-connection-worker.js
    Due to Lerna's hoisting, it's in the top-level node_modules

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ const plugins = [
     new MiniCssExtractPlugin({filename: '../dist/converse.min.css'}),
     new CopyWebpackPlugin({
         patterns: [
-            {from: 'src/headless/node_modules/strophe.js/src/shared-connection-worker.js', to: 'shared-connection-worker.js'},
+            {from: 'node_modules/strophe.js/src/shared-connection-worker.js', to: 'shared-connection-worker.js'},
             {from: 'sounds', to: 'sounds'},
             {from: 'images/favicon.ico', to: 'images/favicon.ico'},
             {from: 'images/custom_emojis', to: 'images/custom_emojis'},

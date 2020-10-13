395aa8cb959bbb7e26472ed3356160c8044be081
JC Brand <jc@opkode.com>
2020-10-13 15:35:54 +0200

    Use JS to dynamically set height of img modal

--- src/modals/image.js
+++ src/modals/image.js
@@ -4,6 +4,9 @@ import tpl_image_modal from "../templates/image_modal.js";
 
 export default BootstrapModal.extend({
     toHTML () {
-        return tpl_image_modal({'src': this.src});
+        return tpl_image_modal({
+            'src': this.src,
+            'onload': ev => (ev.target.parentElement.style.height = `${ev.target.height}px`)
+        });
     }
 });

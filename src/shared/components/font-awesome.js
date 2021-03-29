65742d32ecdf850727ab51ff3a999c228fe543e6
JC Brand <jc@opkode.com>
2021-03-29 13:16:32 +0200

    Move icons template to shared folder

--- src/shared/components/font-awesome.js
+++ src/shared/components/font-awesome.js
@@ -1,5 +1,5 @@
 import { CustomElement } from './element.js';
-import tpl_icons from 'templates/icons.js';
+import tpl_icons from '../templates/icons.js';
 
 export class FontAwesome extends CustomElement {
     render () {  // eslint-disable-line class-methods-use-this

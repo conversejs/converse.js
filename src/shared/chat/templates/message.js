78846ab984277f2ffa688d2e0704ca12d5b82e7b
JC Brand <jc@opkode.com>
2021-11-19 22:55:51 +0100

    Fix: don't show avatars for followup messages

--- src/shared/chat/templates/message.js
+++ src/shared/chat/templates/message.js
@@ -7,6 +7,7 @@ import { shouldRenderMediaFromURL } from '@converse/headless/utils/url.js';
 
 export default (el, o) => {
     const i18n_new_messages = __('New messages');
+    const is_followup = el.model.isFollowup();
     return html`
         ${ o.is_first_unread ? html`<div class="message separator"><hr class="separator"><span class="separator-text">${ i18n_new_messages }</span></div>` : '' }
         <div class="message chat-msg ${ el.getExtraMessageClasses() }"
@@ -18,7 +19,7 @@ export default (el, o) => {
             <!-- Anchor to allow us to scroll the message into view -->
             <a id="${o.msgid}"></a>
 
-            ${ o.should_show_avatar ?
+            ${ (o.should_show_avatar && !is_followup) ?
                 html`<a class="show-msg-author-modal" @click=${el.showUserModal}>
                     <converse-avatar
                         class="avatar align-self-center"
@@ -28,14 +29,14 @@ export default (el, o) => {
                 </a>` : '' }
 
             <div class="chat-msg__content chat-msg__content--${o.sender} ${o.is_me_message ? 'chat-msg__content--action' : ''}">
-
-                ${ !o.is_me_message ? html`
+                ${ (!o.is_me_message && !is_followup) ? html`
                     <span class="chat-msg__heading">
                         <span class="chat-msg__author"><a class="show-msg-author-modal" @click=${el.showUserModal}>${o.username}</a></span>
                         ${ o.hats.map(h => html`<span class="badge badge-secondary">${h.title}</span>`) }
                         <time timestamp="${el.model.get('edited') || el.model.get('time')}" class="chat-msg__time">${o.pretty_time}</time>
                         ${ o.is_encrypted ? html`<span class="fa fa-lock"></span>` : '' }
                     </span>` : '' }
+
                 <div class="chat-msg__body chat-msg__body--${o.message_type} ${o.received ? 'chat-msg__body--received' : '' } ${o.is_delayed ? 'chat-msg__body--delayed' : '' }">
                     <div class="chat-msg__message">
                         ${ (o.is_me_message) ? html`

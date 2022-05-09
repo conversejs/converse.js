9b703c0860335f9954c6322d50d5af1572a31342
HAMANO Tsukasa <code@cuspy.org>
2022-05-09 23:33:40 +0900

    use jed1.x for po2json

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
             {
                 loader: 'po-loader',
                 options: {
-                    'format': 'jed',
+                    'format': 'jed1.x',
                     'domain': 'converse'
                 }
             }

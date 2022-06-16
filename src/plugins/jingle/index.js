5e5fadc223caca920ca83ec82cad8b66d23d2e05
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2022-06-16 19:39:04 +0530

    jingle: added the initial boilerplate code for the plugin.

---
+++ src/plugins/jingle/index.js
@@ -0,0 +1,14 @@
+/**
+ * @description Converse.js plugin which adds XEP-0166 Jingle
+ * @copyright 2022, the Converse.js contributors
+ * @license Mozilla Public License (MPLv2)
+ */
+
+import { converse } from '@converse/headless/core';
+
+converse.plugins.add('jingle', {
+
+    initialize: function () {
+       
+    },
+});

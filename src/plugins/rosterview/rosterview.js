1a8ae3dcbe9fd6ae1b02f739c862628fac1b02ce
JC Brand <jc@opkode.com>
2022-10-02 14:30:16 +0200

    Make roster contacts toggleable

--- src/plugins/rosterview/rosterview.js
+++ src/plugins/rosterview/rosterview.js
@@ -2,6 +2,8 @@ import tpl_roster from "./templates/roster.js";
 import { CustomElement } from 'shared/components/element.js';
 import { Model } from '@converse/skeletor/src/model.js';
 import { _converse, api } from "@converse/headless/core";
+import { initStorage } from '@converse/headless/utils/storage.js';
+import { slideIn, slideOut } from 'utils/html.js';
 
 
 /**
@@ -12,7 +14,13 @@ import { _converse, api } from "@converse/headless/core";
 export default class RosterView extends CustomElement {
 
     async initialize () {
+        const id = `converse.contacts-panel${_converse.bare_jid}`;
+        this.model = new Model({ id });
+        initStorage(this.model, id);
+        this.model.fetch();
+
         await api.waitUntil('rosterInitialized')
+
         const { presences, roster } = _converse;
         this.listenTo(_converse, 'rosterContactsFetched', () => this.requestUpdate());
         this.listenTo(presences, 'change:show', () => this.requestUpdate());
@@ -21,6 +29,7 @@ export default class RosterView extends CustomElement {
         this.listenTo(roster, 'remove', () => this.requestUpdate());
         this.listenTo(roster, 'change', () => this.requestUpdate());
         this.listenTo(roster.state, 'change', () => this.requestUpdate());
+        this.listenTo(this.model, 'change', () => this.requestUpdate());
         /**
          * Triggered once the _converse.RosterView instance has been created and initialized.
          * @event _converse#rosterViewInitialized
@@ -29,17 +38,8 @@ export default class RosterView extends CustomElement {
         api.trigger('rosterViewInitialized');
     }
 
-    firstUpdated () {
-        this.listenToRosterFilter();
-    }
-
     render () {
         return tpl_roster(this);
-    }
-
-    listenToRosterFilter () {
-        this.filter_view = this.querySelector('converse-roster-filter');
-        this.filter_view.addEventListener('update', () => this.requestUpdate());
     }
 
     showAddContactModal (ev) { // eslint-disable-line class-methods-use-this
@@ -48,15 +48,26 @@ export default class RosterView extends CustomElement {
 
     async syncContacts (ev) { // eslint-disable-line class-methods-use-this
         ev.preventDefault();
+        const { roster } = _converse;
         this.syncing_contacts = true;
         this.requestUpdate();
 
-        _converse.roster.data.save('version', null);
-        await _converse.roster.fetchFromServer();
+        roster.data.save('version', null);
+        await roster.fetchFromServer();
         api.user.presence.send();
 
         this.syncing_contacts = false;
         this.requestUpdate();
+    }
+
+    toggleRoster (ev) {
+        ev?.preventDefault?.();
+        const list_el = this.querySelector('.list-container.roster-contacts');
+        if (this.model.get('toggle_state') === _converse.CLOSED) {
+            slideOut(list_el).then(() => this.model.save({'toggle_state': _converse.OPENED}));
+        } else {
+            slideIn(list_el).then(() => this.model.save({'toggle_state': _converse.CLOSED}));
+        }
     }
 }
 

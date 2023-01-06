380ad88666dfcba74a702adfc0f6ab9f2fddc41e
JC Brand <jc@opkode.com>
2023-01-06 14:58:14 +0100

    Remove unused alias

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -109,7 +109,6 @@ module.exports = {
         alias: {
             "IPv6":                     path.resolve(__dirname, "../node_modules/urijs/src/IPv6"),
             "SecondLevelDomains":       path.resolve(__dirname, "../node_modules/urijs/src/SecondLevelDomains"),
-            "formdata-polyfill":        path.resolve(__dirname, "../node_modules/formdata-polyfill/FormData"),
             "punycode":                 path.resolve(__dirname, "../node_modules/urijs/src/punycode"),
             "./shims":                  path.resolve(__dirname, "../src/strophe-shims.js"),
         }

dcbde1f3fa47b6063d9062d7ec8595e997a61ceb
Hippo <badrihippo@disroot.org>
2023-01-18 13:48:53 +0530

    Split 'omemo-views/index.js' into headless and UI plugins
    The 'omemo-views' plugin is still there, but most of the logic has
    now been moved to the new headless 'omemo' plugin, with only the
    UI part remaining in 'omemo-views'.

---
+++ src/headless/plugins/omemo/index.js
@@ -0,0 +1,100 @@
+/**
+ * @copyright The Converse.js contributors
+ * @license Mozilla Public License (MPLv2)
+ */
+import ConverseMixins from './mixins/converse.js';
+import omemo_api from './api.js';
+import Device from './device.js';
+import DeviceList from './devicelist.js';
+import DeviceLists from './devicelists.js';
+import Devices from './devices.js';
+import OMEMOStore from './store.js';
+import { _converse, api, converse } from '@converse/headless/core';
+
+import {
+    createOMEMOMessageStanza,
+    encryptFile,
+    getOutgoingMessageAttributes,
+    handleEncryptedFiles,
+    handleMessageSendError,
+    initOMEMO,
+    omemo,
+    onChatBoxesInitialized,
+    parseEncryptedMessage,
+    registerPEPPushHandler
+    setEncryptedFileURL,
+} from './utils.js'
+
+const { Strophe } = converse.env;
+
+converse.env.omemo = omemo;
+
+Strophe.addNamespace('OMEMO_DEVICELIST', Strophe.NS.OMEMO + '.devicelist');
+Strophe.addNamespace('OMEMO_VERIFICATION', Strophe.NS.OMEMO + '.verification');
+Strophe.addNamespace('OMEMO_WHITELISTED', Strophe.NS.OMEMO + '.whitelisted');
+Strophe.addNamespace('OMEMO_BUNDLES', Strophe.NS.OMEMO + '.bundles');
+
+
+converse.plugins.add('converse-omemo', {
+    enabled (_converse) {
+      return (
+          window.libsignal &&
+          _converse.config.get('trusted') &&
+          !api.settings.get('clear_cache_on_logout') &&
+          !_converse.api.settings.get('blacklisted-plugins').includes('converse-omemo-views')
+      );
+    },
+
+    dependencies: ['converse-chat', 'converse-pubsub'],
+
+    initialize() {
+        api.settings.extend({ 'omemo-default': false });
+        api.promises.add(['OMEMOInitialized']);
+
+        _converse.NUM_PREKEYS = 100; // Set here so that tests can override
+
+        Object.assign(_converse, ConverseMixins);
+        Object.assign(_converse.api, omemo_api);
+
+        _converse.OMEMOStore = OMEMOStore;
+        _converse.Device = Device;
+        _converse.Devices = Devices;
+        _converse.DeviceList = DeviceList;
+        _converse.DeviceLists = DeviceLists;
+
+        /******************** Event Handlers ********************/
+        api.waitUntil('chatBoxesInitialized').then(onChatBoxesInitialized);
+
+        api.listen.on('getOutgoingMessageAttributes', getOutgoingMessageAttributes);
+
+        api.listen.on('createMessageStanza', async (chat, data) => {
+            try {
+                data = await createOMEMOMessageStanza(chat, data);
+            } catch (e) {
+                handleMessageSendError(e, chat);
+            }
+            return data;
+        });
+
+        api.listen.on('afterFileUploaded', (msg, attrs) => msg.file.xep454_ivkey ? setEncryptedFileURL(msg, attrs) : attrs);
+        api.listen.on('beforeFileUpload', (chat, file) => chat.get('omemo_active') ? encryptFile(file) : file);
+
+        api.listen.on('parseMessage', parseEncryptedMessage);
+        api.listen.on('parseMUCMessage', parseEncryptedMessage);
+
+        api.listen.on('connected', registerPEPPushHandler);
+
+        api.listen.on('statusInitialized', initOMEMO);
+        api.listen.on('addClientFeatures', () => api.disco.own.features.add(`${Strophe.NS.OMEMO_DEVICELIST}+notify`));
+
+        api.listen.on('afterMessageBodyTransformed', handleEncryptedFiles);
+
+        api.listen.on('clearSession', () => {
+            delete _converse.omemo_store
+            if (_converse.shouldClearCache() && _converse.devicelists) {
+                _converse.devicelists.clearStore();
+                delete _converse.devicelists;
+            }
+        });
+    }
+});
--- src/plugins/omemo-views/index.js
+++ src/plugins/omemo-views/index.js
@@ -5,47 +5,12 @@
 import './fingerprints.js';
 import './profile.js';
 import 'shared/modals/user-details.js';
-import ConverseMixins from './mixins/converse.js';
-import Device from './device.js';
-import DeviceList from './devicelist.js';
-import DeviceLists from './devicelists.js';
-import Devices from './devices.js';
-import OMEMOStore from './store.js';
 import log from '@converse/headless/log';
-import omemo_api from './api.js';
 import { _converse, api, converse } from '@converse/headless/core';
 import {
-    createOMEMOMessageStanza,
     getOMEMOToolbarButton,
     onChatInitialized,
 } from './utils.js';
-
-// Temporary: to be removed to headless if possible!
-import {
-    // createOMEMOMessageStanza,
-    encryptFile,
-    // getOMEMOToolbarButton,
-    getOutgoingMessageAttributes,
-    handleEncryptedFiles,
-    handleMessageSendError,
-    initOMEMO,
-    omemo,
-    /onChatBoxesInitialized,
-    // onChatInitialized,
-    parseEncryptedMessage,
-    registerPEPPushHandler,
-    setEncryptedFileURL,
-} from '@converse/headless/plugins/omemo/utils.js'
-
-const { Strophe } = converse.env;
-
-converse.env.omemo = omemo;
-
-Strophe.addNamespace('OMEMO_DEVICELIST', Strophe.NS.OMEMO + '.devicelist');
-Strophe.addNamespace('OMEMO_VERIFICATION', Strophe.NS.OMEMO + '.verification');
-Strophe.addNamespace('OMEMO_WHITELISTED', Strophe.NS.OMEMO + '.whitelisted');
-Strophe.addNamespace('OMEMO_BUNDLES', Strophe.NS.OMEMO + '.bundles');
-
 
 converse.plugins.add('converse-omemo-views', {
     enabled (_converse) {
@@ -57,53 +22,13 @@ converse.plugins.add('converse-omemo-views', {
         );
     },
 
-    dependencies: ['converse-chatview', 'converse-pubsub', 'converse-profile'],
+    dependencies: ['converse-chatview', 'converse-pubsub', 'converse-profile', 'converse-omemo'],
 
     initialize () {
-        api.settings.extend({ 'omemo_default': false });
-        api.promises.add(['OMEMOInitialized']);
-
-        _converse.NUM_PREKEYS = 100; // Set here so that tests can override
-
-        Object.assign(_converse, ConverseMixins);
-        Object.assign(_converse.api, omemo_api);
-
-        _converse.OMEMOStore = OMEMOStore;
-        _converse.Device = Device;
-        _converse.Devices = Devices;
-        _converse.DeviceList = DeviceList;
-        _converse.DeviceLists = DeviceLists;
-
-        /******************** Event Handlers ********************/
-        api.waitUntil('chatBoxesInitialized').then(onChatBoxesInitialized);
-
-        api.listen.on('getOutgoingMessageAttributes', getOutgoingMessageAttributes);
-
-        api.listen.on('createMessageStanza', async (chat, data) => {
-            try {
-                data = await createOMEMOMessageStanza(chat, data);
-            } catch (e) {
-                handleMessageSendError(e, chat);
-            }
-            return data;
-        });
-
-        api.listen.on('afterFileUploaded', (msg, attrs) => msg.file.xep454_ivkey ? setEncryptedFileURL(msg, attrs) : attrs);
-        api.listen.on('beforeFileUpload', (chat, file) => chat.get('omemo_active') ? encryptFile(file) : file);
-
-        api.listen.on('parseMessage', parseEncryptedMessage);
-        api.listen.on('parseMUCMessage', parseEncryptedMessage);
-
         api.listen.on('chatBoxViewInitialized', onChatInitialized);
         api.listen.on('chatRoomViewInitialized', onChatInitialized);
 
-        api.listen.on('connected', registerPEPPushHandler);
         api.listen.on('getToolbarButtons', getOMEMOToolbarButton);
-
-        api.listen.on('statusInitialized', initOMEMO);
-        api.listen.on('addClientFeatures', () => api.disco.own.features.add(`${Strophe.NS.OMEMO_DEVICELIST}+notify`));
-
-        api.listen.on('afterMessageBodyTransformed', handleEncryptedFiles);
 
         api.listen.on('userDetailsModalInitialized', contact => {
             const jid = contact.get('jid');
@@ -112,14 +37,6 @@ converse.plugins.add('converse-omemo-views', {
 
         api.listen.on('profileModalInitialized', () => {
             _converse.generateFingerprints(_converse.bare_jid).catch(e => log.error(e));
-        });
-
-        api.listen.on('clearSession', () => {
-            delete _converse.omemo_store
-            if (_converse.shouldClearCache() && _converse.devicelists) {
-                _converse.devicelists.clearStore();
-                delete _converse.devicelists;
-            }
         });
     }
 });

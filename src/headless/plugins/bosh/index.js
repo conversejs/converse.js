9b792acf70be828694e79b66ab2a196ad47278c4
JC Brand <jc@opkode.com>
2023-11-10 13:53:46 +0100

    Move BOSH plugin into its own folder

---
+++ src/headless/plugins/bosh/index.js
@@ -0,0 +1,35 @@
+/**
+ * @copyright The Converse.js contributors
+ * @license Mozilla Public License (MPLv2)
+ * @description Converse.js plugin which add support for XEP-0206: XMPP Over BOSH
+ */
+import 'strophe.js/src/bosh';
+import { Strophe } from "strophe.js";
+import _converse from '../../shared/_converse.js';
+import api, { converse } from '../../shared/api/index.js';
+import bosh_api from './api.js';
+import { attemptPrebind, clearSession, saveJIDToSession} from './utils.js';
+
+
+converse.plugins.add('converse-bosh', {
+
+    enabled () {
+        return !_converse.api.settings.get("blacklisted_plugins").includes('converse-bosh');
+    },
+
+    initialize () {
+        api.settings.extend({
+            bosh_service_url: undefined,
+            prebind_url: null
+        });
+
+        Object.assign(api, bosh_api);
+
+        api.listen.on('clearSession', clearSession);
+        api.listen.on('setUserJID', saveJIDToSession);
+        api.listen.on('login', attemptPrebind);
+        api.listen.on('addClientFeatures',
+            () => api.disco.own.features.add(Strophe.NS.BOSH)
+        );
+    }
+});

36048d857900cd14a3ba18f929cf4afbecdf137c
JC Brand <jc@opkode.com>
2024-05-24 10:59:44 +0200

    Bugfix: Make sure form type is properly rendered
    Otherwise we get the error: `Form is not of type room configuration`

--- src/types/plugins/profile/statusview.d.ts
+++ src/types/plugins/profile/statusview.d.ts
@@ -1,2 +1,11 @@
-export {};
+export default Profile;
+declare class Profile extends CustomElement {
+    initialize(): void;
+    model: any;
+    render(): import("lit-html").TemplateResult<1>;
+    showProfileModal(ev: any): void;
+    showStatusChangeModal(ev: any): void;
+    showUserSettingsModal(ev: any): void;
+}
+import { CustomElement } from "shared/components/element.js";
 //# sourceMappingURL=statusview.d.ts.map

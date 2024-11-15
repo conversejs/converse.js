851f58227f1bee7702ba8b1c6a80b170caa4836d
JC Brand <jc@opkode.com>
2024-11-15 12:14:41 +0200

    Only show chat dragresize in overlayed view mode

--- src/plugins/muc-views/templates/muc.js
+++ src/plugins/muc-views/templates/muc.js
@@ -1,11 +1,12 @@
+import { html } from "lit";
+import { api } from '@converse/headless';
+import { getChatRoomBodyTemplate } from '../utils.js';
 import '../chatarea.js';
 import '../destroyed.js';
 import '../disconnected.js';
 import '../heading.js';
 import '../nickname-form.js';
 import '../password-form.js';
-import { html } from "lit";
-import { getChatRoomBodyTemplate } from '../utils.js';
 
 
 /**
@@ -14,7 +15,7 @@ import { getChatRoomBodyTemplate } from '../utils.js';
 export default (el) => {
     return html`
         <div class="flyout box-flyout">
-            <converse-dragresize></converse-dragresize>
+            ${ api.settings.get('view_mode') === 'overlayed' ? html`<converse-dragresize></converse-dragresize>` : '' }
             ${ el.model ? html`
                 <converse-muc-heading jid="${el.model.get('jid')}" class="chat-head chat-head-chatroom row g-0">
                 </converse-muc-heading>

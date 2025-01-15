269ff4444ba27091839a270cd46fe10d7a25693d
JC Brand <jc@opkode.com>
2025-01-15 15:54:33 +0200

    Don't show empty <a> for video

--- src/templates/video.js
+++ src/templates/video.js
@@ -8,6 +8,6 @@ export default (url, hide_url) => {
     const { hostname } = new URL(url);
     return html`<figure>
         <video controls preload="metadata" src="${url}"></video>
-        ${hide_url ? '' : html`<a target="_blank" rel="noopener" title="${url}" href="${url}">${hostname}</a>`}
+        ${hide_url || !hostname ? '' : html`<a target="_blank" rel="noopener" title="${url}" href="${url}">${hostname}</a>`}
     </figure>`;
 }

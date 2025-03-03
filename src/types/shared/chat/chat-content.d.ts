ccc4afea33ced05bd72ee4bc3a5ced359b48d479
JC Brand <jc@opkode.com>
2025-03-03 23:10:47 +0200

    feature: support a virtualized chat history
    Fixes #2335

--- src/types/shared/chat/chat-content.d.ts
+++ src/types/shared/chat/chat-content.d.ts
@@ -1,14 +1,30 @@
+/**
+ * Implements a virtualized list of chat messages, which means only a subset of
+ * messages, the `WINDOW_SIZE`, gets rendered to the DOM, and this subset
+ * gets updated as the user scrolls up and down.
+ */
 export default class ChatContent extends CustomElement {
     static get properties(): {
         model: {
             type: ObjectConstructor;
         };
+        window_top: {
+            state: boolean;
+        };
+        window_bottom: {
+            state: boolean;
+        };
     };
     model: any;
-    connectedCallback(): void;
+    scroll_debounce: any;
+    window_top: number;
+    window_bottom: number;
+    scrollHandler: (ev: Event) => void;
+    mark_scrolled_debounce: NodeJS.Timeout;
     initialize(): Promise<void>;
     render(): import("lit").TemplateResult<1> | "";
     scrollDown(): void;
+    #private;
 }
-import { CustomElement } from '../components/element.js';
+import { CustomElement } from "../components/element.js";
 //# sourceMappingURL=chat-content.d.ts.map

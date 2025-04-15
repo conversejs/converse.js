e04390f0138e68f6c3607a3298e0ce9ca980424b
JC Brand <jc@opkode.com>
2025-04-15 08:41:59 +0200

    Run prettier

--- src/plugins/controlbox/buttons.js
+++ src/plugins/controlbox/buttons.js
@@ -4,34 +4,28 @@ import tplButtons from './templates/buttons.js';
 
 import './styles/buttons.scss';
 
-
 class ControlboxButtons extends CustomElement {
-    initialize () {
+    initialize() {
         const { chatboxes } = _converse.state;
         this.model = chatboxes.get('controlbox');
     }
 
-    render () {
+    render() {
         return tplButtons(this);
     }
 
-
     /**
      * @param {MouseEvent} ev
      */
-    showUserSettingsModal (ev) {
+    showUserSettingsModal(ev) {
         ev?.preventDefault();
-        api.modal.show(
-            'converse-user-settings-modal',
-            { model: _converse.state.xmppstatus, _converse },
-            ev
-        );
+        api.modal.show('converse-user-settings-modal', { model: _converse.state.xmppstatus, _converse }, ev);
     }
 
     /**
      * @param {MouseEvent} ev
      */
-    closeControlBox (ev) {
+    closeControlBox(ev) {
         ev?.preventDefault();
         const view = _converse.state.chatboxviews.get('controlbox');
         view?.close();
--- src/plugins/controlbox/templates/buttons.js
+++ src/plugins/controlbox/templates/buttons.js
@@ -1,25 +1,25 @@
-import { html } from "lit";
-import { api } from "@converse/headless";
+import { html } from 'lit';
+import { api } from '@converse/headless';
 import { __ } from 'i18n';
 import { logOut } from '../utils.js';
 
-
-function tplSignout () {
+function tplSignout() {
     const i18n_logout = __('Log out');
     return html`<a class="controlbox-heading__btn logout align-self-center" title="${i18n_logout}" @click=${logOut}>
         <converse-icon class="fa fa-sign-out-alt" size="1em"></converse-icon>
-    </a>`
+    </a>`;
 }
 
 /**
  * @param {import('../buttons').default} el
  */
-function tplUserSettingsButton (el) {
+function tplUserSettingsButton(el) {
     const i18n_details = __('Show details about this chat client');
-    return html`<a class="controlbox-heading__btn show-client-info align-self-center"
+    return html`<a
+        class="controlbox-heading__btn show-client-info align-self-center"
         title="${i18n_details}"
-        @click=${el.showUserSettingsModal}>
-
+        @click=${el.showUserSettingsModal}
+    >
         <converse-icon class="fa fa-cog" size="1em"></converse-icon>
     </a>`;
 }
@@ -27,11 +27,10 @@ function tplUserSettingsButton (el) {
 /**
  * @param {import('../buttons').default} el
  */
-function tplCloseButton (el) {
-    return html`
-        <a class="controlbox-heading__btn close align-self-center" @click=${(ev) => el.closeControlBox(ev)}>
-            <converse-icon class="fa fa-times" size="1em"></converse-icon>
-        </a>`
+function tplCloseButton(el) {
+    return html` <a class="controlbox-heading__btn close align-self-center" @click=${(ev) => el.closeControlBox(ev)}>
+        <converse-icon class="fa fa-times" size="1em"></converse-icon>
+    </a>`;
 }
 
 /**
@@ -40,10 +39,9 @@ function tplCloseButton (el) {
 export default (el) => {
     const is_connected = el.model.get('connected');
     const show_settings_button = api.settings.get('show_client_info') || api.settings.get('allow_adhoc_commands');
-    return html`
-        <div class="btn-toolbar g-0">
-            ${is_connected && show_settings_button  ? tplUserSettingsButton(el) : ''}
-            ${is_connected && api.settings.get('allow_logout') ? tplSignout() : ''}
-            ${api.settings.get('sticky_controlbox') ? '' : tplCloseButton(el)}
-        </div>`;
+    return html` <div class="btn-toolbar g-0">
+        ${is_connected && show_settings_button ? tplUserSettingsButton(el) : ''}
+        ${is_connected && api.settings.get('allow_logout') ? tplSignout() : ''}
+        ${api.settings.get('sticky_controlbox') ? '' : tplCloseButton(el)}
+    </div>`;
 };

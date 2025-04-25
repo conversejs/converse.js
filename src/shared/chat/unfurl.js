09d135e3c049f9083261175ab5064ace13f7aa18
JC Brand <jc@opkode.com>
2025-04-25 07:57:31 +0200

    Prettier

--- src/shared/chat/unfurl.js
+++ src/shared/chat/unfurl.js
@@ -1,23 +1,21 @@
-import { api } from "@converse/headless";
+import { api } from '@converse/headless';
 import { CustomElement } from 'shared/components/element.js';
 import tplUnfurl from './templates/unfurl.js';
 
 import './styles/unfurl.scss';
 
-
 export default class MessageUnfurl extends CustomElement {
-
-    static get properties () {
+    static get properties() {
         return {
             description: { type: String },
             image: { type: String },
             jid: { type: String },
             title: { type: String },
             url: { type: String },
-        }
+        };
     }
 
-    constructor () {
+    constructor() {
         super();
         this.jid = null;
         this.url = null;
@@ -26,24 +24,29 @@ export default class MessageUnfurl extends CustomElement {
         this.description = null;
     }
 
-    initialize () {
+    initialize() {
         const settings = api.settings.get();
         this.listenTo(settings, 'change:allowed_image_domains', () => this.requestUpdate());
         this.listenTo(settings, 'change:render_media', () => this.requestUpdate());
     }
 
-    render () {
-        return tplUnfurl(Object.assign({
-            'onload': () => this.onImageLoad()
-        }, {
-            description: this.description || '',
-            image: this.image || '',
-            title: this.title || '',
-            url: this.url || ''
-        }));
+    render() {
+        return tplUnfurl(
+            Object.assign(
+                {
+                    'onload': () => this.onImageLoad(),
+                },
+                {
+                    description: this.description || '',
+                    image: this.image || '',
+                    title: this.title || '',
+                    url: this.url || '',
+                }
+            )
+        );
     }
 
-    onImageLoad () {
+    onImageLoad() {
         this.dispatchEvent(new CustomEvent('imageLoaded', { detail: this, 'bubbles': true }));
     }
 }

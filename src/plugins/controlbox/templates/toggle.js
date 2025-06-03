0ecda99d1694bf7da1d4ba099465deee91dd5126
JC Brand <jc@opkode.com>
2025-06-03 12:38:24 +0200

    Rename chat toggle to just `Chat`

--- src/plugins/controlbox/templates/toggle.js
+++ src/plugins/controlbox/templates/toggle.js
@@ -1,5 +1,4 @@
-import { html } from "lit";
-import { api } from "@converse/headless";
+import { html } from 'lit';
 import { __ } from 'i18n';
 import { showControlBox } from '../utils.js';
 
@@ -7,10 +6,11 @@ import { showControlBox } from '../utils.js';
  * @param {import('../toggle').default} el
  */
 export default (el) => {
-    const i18n_toggle = api.connection.connected() ? __('Chat Contacts') : __('Toggle chat');
-    return html`<button type="button"
-            class="btn toggle-controlbox ${el.model?.get('closed') ? '' : 'hidden'}"
-            @click=${(ev) => showControlBox(ev)}>
-        <span class="toggle-feedback">${i18n_toggle}</span>
+    return html`<button
+        type="button"
+        class="btn toggle-controlbox ${el.model?.get('closed') ? '' : 'hidden'}"
+        @click=${(ev) => showControlBox(ev)}
+    >
+        <span class="toggle-feedback">${__('Chat')}</span>
     </button>`;
-}
+};

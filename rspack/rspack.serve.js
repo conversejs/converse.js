0d4007efb520949ac62e1f70e4f19c779578d6f3
JC Brand <jc@opkode.com>
2025-08-09 23:17:42 +0200

    RSPack updates
    Add converse-headless as entry point

--- rspack/rspack.serve.js
+++ rspack/rspack.serve.js
@@ -5,24 +5,49 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
     mode: 'development',
     entry: {
-        'converse': path.resolve(__dirname, '../src/entry.js'),
+        'converse': {
+            import: path.resolve(__dirname, '../src/entry.js'),
+            filename: '../dist/[name].js'
+        },
+        'converse-headless': {
+            import: path.resolve(__dirname, '../src/headless/index.js'),
+            filename: '../src/headless/dist/[name].js'
+        },
     },
     devtool: 'inline-source-map',
     optimization: {
         minimize: false,
     },
     devServer: {
-        static: {
-            directory: path.resolve(__dirname, '../'),
-        },
+        static: [
+            {
+                directory: path.resolve(__dirname, '..'), // Serve root directory for dev.html etc
+                publicPath: '/',
+                serveIndex: true,
+                watch: {
+                    ignored: ['**/node_modules', '**/dist', '**/.git']
+                }
+            },
+            {
+                directory: path.resolve(__dirname, '../dist'),
+                publicPath: '/dist/'
+            },
+            {
+                directory: path.resolve(__dirname, '../src/headless/dist'),
+                publicPath: '/src/headless/dist/'
+            }
+        ],
         port: 8008,
         allowedHosts: ['localhost'],
         devMiddleware: {
-            publicPath: '/dist/',
             writeToDisk: true,
         },
     },
     watchOptions: {
-        ignored: [path.posix.resolve(__dirname, '../node_modules'), path.posix.resolve(__dirname, '../3rdparty')],
+        ignored: [
+            path.posix.resolve(__dirname, '../node_modules'),
+            path.posix.resolve(__dirname, '../3rdparty'),
+            path.posix.resolve(__dirname, '../src/headless/dist') // Don't watch the output dir
+        ],
     },
 });

8ed4a429ace18104d90ab79474b06cf02542645b
JC Brand <jc@opkode.com>
2025-08-25 13:05:13 +0200

    Update controlbox to not show logo, navbar and version while connecting

--- src/plugins/controlbox/templates/controlbox.js
+++ src/plugins/controlbox/templates/controlbox.js
@@ -11,21 +11,27 @@ const { ANONYMOUS } = constants;
 function whenNotConnected(el) {
     const is_fullscreen = api.settings.get('view_mode') === 'fullscreen';
     const connection_status = _converse.state.connfeedback.get('connection_status');
-    const connecting = [Strophe.Status.RECONNECTING, Strophe.Status.CONNECTING].includes(connection_status);
+    const connecting = [Strophe.Status.RECONNECTING, Strophe.Status.CONNECTING, Strophe.Status.CONNECTED].includes(
+        connection_status
+    );
     const view_mode = api.settings.get('view_mode');
     const show_bg = api.settings.get('show_background');
+
     return html`
         ${show_bg && view_mode === 'fullscreen' ? html`<converse-bg></converse-bg>` : ''}
         <converse-controlbox-buttons class="controlbox-padded"></converse-controlbox-buttons>
         <div class="controlbox-pane d-flex flex-column justify-content-between">
-            ${is_fullscreen ? html`<converse-controlbox-navbar></converse-controlbox-navbar>` : ''}
-            <converse-brand-logo></converse-brand-logo>
             ${connecting
                 ? html`<converse-spinner class="vertically-centered fade-in"></converse-spinner>`
                 : el.model.get('active-form') === 'register'
-                  ? html`<converse-registration-form class="fade-in rounded"></converse-registration-form>`
-                  : html`<converse-login-form class="fade-in rounded"></converse-login-form>`}
-            ${is_fullscreen ? html`<converse-footer></converse-footer>` : ''}
+                  ? html`${is_fullscreen ? html`<converse-controlbox-navbar></converse-controlbox-navbar>` : ''}
+                        <converse-brand-logo></converse-brand-logo>
+                        <converse-registration-form class="fade-in rounded"></converse-registration-form>
+                        ${is_fullscreen ? html`<converse-footer></converse-footer>` : ''} `
+                  : html`${is_fullscreen ? html`<converse-controlbox-navbar></converse-controlbox-navbar>` : ''}
+                        <converse-brand-logo></converse-brand-logo>
+                        <converse-login-form class="fade-in rounded"></converse-login-form>
+                        ${is_fullscreen ? html`<converse-footer></converse-footer>` : ''} `}
         </div>
     `;
 }

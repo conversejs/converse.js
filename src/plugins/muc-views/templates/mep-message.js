8835a1c58251a3c1709ba6996d0d160d8c2c64f0
subhu339 <sk3656770@gmail.com>
2026-01-23 18:07:21 +0530

    Add reply preview UI and highlight animation

--- src/plugins/muc-views/templates/mep-message.js
+++ src/plugins/muc-views/templates/mep-message.js
@@ -7,6 +7,7 @@ export default (el) => {
     const isodate = dayjs(el.model.get('time')).toISOString();
     return html`
         <div class="message chat-info message--mep ${ el.getExtraMessageClasses() }"
+            data-msgid="${el.model.get('id')}"
             data-isodate="${isodate}"
             data-type="${el.data_name}"
             data-value="${el.data_value}">
@@ -14,16 +15,24 @@ export default (el) => {
             <div class="chat-msg__content">
                 <div class="chat-msg__body chat-msg__body--${el.model.get('type')} ${el.model.get('is_delayed') ? 'chat-msg__body--delayed' : '' }">
                     <div class="chat-info__message">
-                        ${ el.model.isRetracted() ? el.renderRetraction() : html`
-                            <converse-texture
-                                .mentions=${el.model.get('references')}
-                                render_styling
-                                text=${el.model.getMessageText()}>
-                            </converse-texture>
-                            ${ el.model.get('reason') ?
-                                html`<q class="reason"><converse-texture text=${el.model.get('reason')}></converse-texture></q>` : `` }
-                        `}
-                    </div>
+
+    ${ el.hasReply() ? html`
+        <div class="reply-preview" @click=${ev => el.onReplyClick(ev)}>
+            <div class="reply-author">${el.getReplyAuthorName()}</div>
+            <div class="reply-snippet">${el.getReplySnippet()}</div>
+        </div>
+    ` : `` }
+
+    ${ el.model.isRetracted() ? el.renderRetraction() : html`
+        <converse-texture
+            .mentions=${el.model.get('references')}
+            render_styling
+            text=${el.model.getMessageText()}>
+        </converse-texture>
+        ${ el.model.get('reason') ?
+            html`<q class="reason"><converse-texture text=${el.model.get('reason')}></converse-texture></q>` : `` }
+    `}
+</div>
                     <converse-message-actions
                         ?is_retracted=${el.model.isRetracted()}
                         .model=${el.model}></converse-message-actions>

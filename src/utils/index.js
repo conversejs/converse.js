e04e6d8310f5940d2a937016bba8a3195eee74fd
JC Brand <jc@opkode.com>
2026-02-15 23:00:41 +0200

    chore: create non-headless form utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,14 @@
-import { u } from "@converse/headless";
-import * as color from "./color.js";
-import * as file from "./file.js";
-import * as html from "./html.js";
-import * as url from "./url.js";
+import { u } from '@converse/headless';
+import * as color from './color.js';
+import * as file from './file.js';
+import * as form from './form.js';
+import * as html from './html.js';
+import * as url from './url.js';
 
-export default Object.assign(
-    u,
-    {
-        ...color,
-        ...file,
-        ...html,
-        ...url,
-    },
-);
+export default Object.assign(u, {
+    ...color,
+    ...file,
+    ...form,
+    ...html,
+    ...url,
+});

290ce55cc63add645af19f3ac155a7c35dbb1e49
Aiden Woodruff <aiden@aidenw.net>
2026-02-23 23:10:31 -0500

    trim textarea form label whitespace
    - src/templates/form_textarea.js: trim label whitespace from textarea
      forms that have no description.
    - discovered the issue when making new test case.
    
    Signed-off-by: Aiden Woodruff <aiden@aidenw.net>

--- src/templates/form_textarea.js
+++ src/templates/form_textarea.js
@@ -3,11 +3,11 @@ import { u } from '@converse/headless';
 
 export default (o) => {
     const id = u.getUniqueId();
+    const desc = o.desc ? html`
+        <small class="form-text text-muted">${o.desc}</small>` : '';
     return html`
         <div class="mb-3">
-            <label class="form-label label-ta" for="${o.id}">${o.label}
-                ${(o.desc) ? html`<small class="form-text text-muted">${o.desc}</small>` : ''}
-            </label>
+            <label class="form-label label-ta" for="${o.id}">${o.label}${desc}</label>
             <textarea name="${o.name}"
                       id="${id}"
                       ?readonly=${o.readonly}

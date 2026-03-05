195c4d52e68c1489285b046b7787c2d0ef8af778
Merci Jacob <mercihabam@gmail.com>
2026-03-05 15:32:19 +0200

    ensure interactivity on pinned bookmarks and synchronous state update on 'pinned' value change

--- src/plugins/bookmark-views/components/bookmarks-pin-list.js
+++ src/plugins/bookmark-views/components/bookmarks-pin-list.js
@@ -1,14 +1,20 @@
 import { _converse, api } from '@converse/headless';
-import { CustomElement } from "shared/components/element"
 import tplBookmarksPinList from './templates/pin-list';
 import BookmarksPinListModel from './model';
+import { RoomsList } from 'plugins/roomslist/view';
 
-export class BookmarksPinView extends CustomElement {
+export class BookmarksPinView extends RoomsList {
+    model = null;
+
     initialize() {
         const bare_jid = _converse.session.get('bare_jid');
         const id = `converse.bookmarks-pin-list-model-${bare_jid}`;
         this.model = new BookmarksPinListModel({ id });
         _converse.state.bookmarksPinList = this.model;
+
+        this.handleEvents();
+
+        this.requestUpdate();
     }
 
     /** @returns {import('@converse/headless').MUC[]} */

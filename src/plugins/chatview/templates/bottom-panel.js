fcee6a44e723273b0093225461547975d22b6b7e
JC Brand <jc@opkode.com>
2026-03-20 23:15:53 +0200

    chore: move `converse-reply-preview` to bottom panel

--- src/plugins/chatview/templates/bottom-panel.js
+++ src/plugins/chatview/templates/bottom-panel.js
@@ -1,12 +1,21 @@
 import { __ } from 'i18n';
 import { html } from 'lit';
 
-
-export default (o) => {
+/**
+ * @param {import('../bottom-panel').default} el
+ */
+export default (el) => {
     const unread_msgs = __('You have unread messages');
     return html`
-        ${ o.model.ui.get('scrolled') && o.model.get('num_unread') ?
-            html`<div class="new-msgs-indicator" @click=${(ev) => o.viewUnreadMessages(ev)}>▼ ${ unread_msgs } ▼</div>` : '' }
-        <converse-message-form .model=${o.model}></converse-message-form>
+        ${el.model.ui.get('scrolled') && el.model.get('num_unread')
+            ? html`<div
+                  class="new-msgs-indicator"
+                  @click=${/** @param {MouseEvent} ev */ (ev) => el.viewUnreadMessages(ev)}
+              >
+                  ▼ ${unread_msgs} ▼
+              </div>`
+            : ''}
+        <converse-reply-preview .model=${el.model}></converse-reply-preview>
+        <converse-message-form .model=${el.model}></converse-message-form>
     `;
-}
+};

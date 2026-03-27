c624c4ceb48ef0b35229b4ab9bc61ec483db5484
JC Brand <jc@opkode.com>
2026-03-27 08:46:55 +0200

    refactor: share EmojiPicker instance via chatbox property
    Store the EmojiPicker model on the chatbox as a shared plain property
    instead of creating ephemeral instances in the reaction picker. This
    ensures UI state (current category, skintone, scroll position) is
    shared between the toolbar emoji dropdown and the reaction picker.

--- src/shared/chat/emoji-dropdown.js
+++ src/shared/chat/emoji-dropdown.js
@@ -1,45 +1,48 @@
-import { html } from "lit";
-import { _converse, api, u, EmojiPicker, constants } from "@converse/headless";
+import { html } from 'lit';
+import { _converse, api, u, EmojiPicker, constants } from '@converse/headless';
 import { __ } from 'i18n';
-import DropdownBase from "shared/components/dropdown.js";
+import DropdownBase from 'shared/components/dropdown.js';
 import { until } from 'lit/directives/until.js';
 
 const { CHATROOMS_TYPE } = constants;
 const { initStorage } = u;
 
-
 export default class EmojiDropdown extends DropdownBase {
-
     static get properties() {
         return {
             icon_classes: { type: String },
             items: { type: Array },
-            model: { type: Object }
+            model: { type: Object },
         };
     }
 
-    constructor () {
+    constructor() {
         super();
         this.id = u.getUniqueId();
 
         // This is an optimization, we lazily render the emoji picker, otherwise tests slow to a crawl.
         this.render_emojis = false;
         this.state = null;
-        this.model= null;
+        this.model = null;
         this.addEventListener('shown.bs.dropdown', () => this.onShown());
     }
 
-    initModel () {
+    initModel() {
         if (!this.init_promise) {
             this.init_promise = (async () => {
-                await api.emojis.initialize()
-                const bare_jid = _converse.session.get('bare_jid');
-                const id = `converse.emoji-${bare_jid}-${this.model.get('jid')}`;
-                this.state = new EmojiPicker({ id });
-                initStorage(this.state, id);
-                await new Promise(resolve => this.state.fetch({'success': resolve, 'error': resolve}));
+                await api.emojis.initialize();
+                if (!this.model.emoji_picker) {
+                    const bare_jid = _converse.session.get('bare_jid');
+                    const id = `converse.emoji-${bare_jid}-${this.model.get('jid')}`;
+                    this.model.emoji_picker = new EmojiPicker({ id });
+                    initStorage(this.model.emoji_picker, id);
+                    await new Promise((resolve) =>
+                        this.model.emoji_picker.fetch({ 'success': resolve, 'error': resolve }),
+                    );
+                }
+                this.state = this.model.emoji_picker;
                 // We never want still be in the autocompleting state upon page load
-                this.state.set({'autocompleting': null, 'ac_position': null});
+                this.state.set({ 'autocompleting': null, 'ac_position': null });
             })();
         }
         return this.init_promise;
@@ -49,42 +52,49 @@ export default class EmojiDropdown extends DropdownBase {
         const is_groupchat = this.model.get('type') === CHATROOMS_TYPE;
         const color = is_groupchat ? '--muc-color' : '--chat-color';
 
-        return html`
-            <button class="btn dropdown-toggle dropdown-toggle--no-caret toggle-emojis"
-                    type="button"
-                    id="${this.id}"
-                    title="${__('Insert emojis')}"
-                    data-bs-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false">
+        return html` <button
+                class="btn dropdown-toggle dropdown-toggle--no-caret toggle-emojis"
+                type="button"
+                id="${this.id}"
+                title="${__('Insert emojis')}"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+            >
                 <converse-icon
                     color="var(${color})"
                     class="fa fa-smile "
                     path-prefix="${api.settings.get('assets_path')}"
-                    size="1em"></converse-icon>
+                    size="1em"
+                ></converse-icon>
             </button>
             <ul class="dropdown-menu" aria-labelledby="${this.id}">
                 <li>
-                ${until(this.initModel().then(() => html`
-                    <converse-emoji-picker
-                        .state=${this.state}
-                        .model=${this.model}
-                        @emojiSelected=${() => this.dropdown.hide()}
-                        ?render_emojis=${this.render_emojis}
-                        current_category="${this.state.get('current_category') || ''}"
-                        current_skintone="${this.state.get('current_skintone') || ''}"
-                        query="${this.state.get('query') || ''}"
-                    ></converse-emoji-picker>`), '')}
+                    ${until(
+                        this.initModel().then(
+                            () =>
+                                html` <converse-emoji-picker
+                                    .state=${this.state}
+                                    .model=${this.model}
+                                    @emojiSelected=${() => this.dropdown.hide()}
+                                    ?render_emojis=${this.render_emojis}
+                                    current_category="${this.state.get('current_category') || ''}"
+                                    current_skintone="${this.state.get('current_skintone') || ''}"
+                                    query="${this.state.get('query') || ''}"
+                                ></converse-emoji-picker>`,
+                        ),
+                        '',
+                    )}
                 </li>
             </ul>`;
     }
 
-    connectedCallback () {
+    connectedCallback() {
         super.connectedCallback();
         this.render_emojis = false;
     }
 
-    async onShown () {
+    async onShown() {
         await this.initModel();
         if (!this.render_emojis) {
             // Trigger an update so that emojis are rendered
@@ -92,7 +102,7 @@ export default class EmojiDropdown extends DropdownBase {
             this.requestUpdate();
             await this.updateComplete;
         }
-        setTimeout(() => /** @type {HTMLInputElement} */(this.querySelector('.emoji-search'))?.focus());
+        setTimeout(() => /** @type {HTMLInputElement} */ (this.querySelector('.emoji-search'))?.focus());
     }
 }
 

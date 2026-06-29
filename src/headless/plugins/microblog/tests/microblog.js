242596c69306443922ea68d585c95e731a6e4810
JC Brand <jc@opkode.com>
2026-06-29 11:46:47 +0200

    tests: consolidate duplicated test helpers

--- src/headless/plugins/microblog/tests/microblog.js
+++ src/headless/plugins/microblog/tests/microblog.js
@@ -1,45 +1,9 @@
 import { describe, it, expect, vi } from 'vitest';
 import mock from '../../../tests/mock.js';
 import converse from '../../../dist/converse-headless.js';
+import { ATOM, MICROBLOG_NODE, PUBSUB_EVENT, makePostStanza, receive } from './utils.js';
 
-const { Strophe, stx, u } = converse.env;
-
-const ATOM = 'http://www.w3.org/2005/Atom';
-const PUBSUB_EVENT = `${Strophe.NS.PUBSUB}#event`;
-const MICROBLOG_NODE = 'urn:xmpp:microblog:0';
-
-/**
- * Inject an incoming PEP/PubSub event stanza, as if pushed by the server.
- * @param {any} _converse
- * @param {Element} stanza
- */
-function receive(_converse, stanza) {
-    _converse.api.connection.get()._dataRecv(mock.createRequest(_converse, stanza));
-}
-
-/**
- * Build a headline PEP event carrying a single plain-text microblog post.
- * @param {string} from - The publisher's bare JID.
- * @param {string} id - The PubSub item id.
- * @param {string} body - The post body.
- */
-function makePostStanza(from, id, body) {
-    return stx`
-        <message xmlns="jabber:client" from="${from}" to="${from}" type="headline">
-          <event xmlns="${PUBSUB_EVENT}">
-            <items node="${MICROBLOG_NODE}">
-              <item id="${id}" publisher="${from}">
-                <entry xmlns="${ATOM}">
-                  <title type="text">${body}</title>
-                  <id>tag:montague.lit,2024-01-01:posts-${id}</id>
-                  <published>2024-01-01T18:30:02Z</published>
-                  <updated>2024-01-01T18:30:02Z</updated>
-                </entry>
-              </item>
-            </items>
-          </event>
-        </message>`;
-}
+const { stx, u } = converse.env;
 
 describe('The microblog plugin', function () {
     it(
